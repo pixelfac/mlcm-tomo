a2c38c896b6295afeb200583a180ca4e6254fcc6
pixelfac <n.harris1@ufl.edu>
2024-02-16 21:31:31 -0500

    finish detector point calcs

--- main.cpp
+++ main.cpp
@@ -15,8 +15,8 @@ public:
 class CTScanner {
 public:
     int views; //number of views, spaced equally around the subject (e.g. 36 views is one every 10 degrees)
-    double sourceDist; //distance from source ray emitter to center of subject. MUST be greater than furthest subject distance from origin
-    double detectorDist; //distance from center detector to center of subject. MUST be greater than furthest subject distance from origin
+    double sourceDist; //distance from source ray emitter to center of subject. MUST be greater than subject diagonal
+    double detectorDist; //distance from center detector to center of subject. MUST be greater than subject diagonal
     double detectorPanelWidth; //total width of entire detector panel
     int numDetectors; //number of equal-sized detectors on detector panel.
 
@@ -32,15 +32,26 @@ public:
         return rotate(initialPosition, degreesRotated);
     }
 
-    //returns a coordinate pair for the position of the detector, factoring in the rotation from the current view and position along detector panel
-    pair<double, double> GetCurrentSourcePosition(int viewNum, int detectorNum) {
+    //returns a pair of coordinates for the position of the detector, factoring in the rotation from the current view and position along detector panel
+    // INPUT: detectorNum is 0-indexed, so #0 means left side of detector
+    // RETURN: first pair is leftmost coord, second pair is right side of detector
+    pair<pair<double, double>, pair<double, double>> GetCurrentDetectorPosition(int viewNum, int detectorNum) {
         pair<double, double> detectorPanelCenter(0.0, detectorDist);
+        pair<double, double> detectorLeftSide(0.0, detectorDist);  //leftmost range of this detector pixel
+        pair<double, double> detectorRightSide(0.0, detectorDist); //rightmost range of this detector pixel
+
+        double detectorWidth = detectorPanelWidth / numDetectors; //width of each individual detector pixel
+        detectorLeftSide.first = (detectorWidth * detectorNum) - (detectorPanelWidth / 2);
+        detectorRightSide.first = detectorLeftSide.first + detectorWidth;
         
         // if viewNum == 0, then source has not moved from initial position, (0, detectorDist)
         double degreesRotated = viewNum * (360.0 / views);
 
         //TODO how to make rotation matrix to rotate a vector by a set number of degrees/radians?
-        return rotate(initialPosition, degreesRotated);
+        detectorLeftSide = rotate(detectorLeftSide, degreesRotated);
+        detectorRightSide = rotate(detectorRightSide, degreesRotated);
+
+        return pair<pair<double, double>, pair<double, double>>(detectorLeftSide, detectorRightSide);
     }
 
     //simulate a fan of photons from source, through subject, and hitting a specific detector
@@ -49,6 +60,10 @@ public:
     void computeFanIntersections(int viewNum, int detectorNum) {
         //compute fanbeam triangle using source position, detector panel position, and which detector on the panel is receiving
         pair<double, double> sourcePosition = GetCurrentSourcePosition(viewNum);
+
+        auto detectorPosition = GetCurrentDetectorPosition(viewNum, detectorNum);
+
+        //using sourcePosition, and both coords in detectorPosition, can make triangle
 
     }
 };
