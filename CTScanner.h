4593808775b586c4745d9860d7ed1dc1d674c9d1
pixelfac <n.harris1@ufl.edu>
2024-03-02 21:10:00 -0500

    add more robust parts of algorithm

--- CTScanner.h
+++ CTScanner.h
@@ -33,7 +33,6 @@ public:
     // INPUT: detectorNum is 0-indexed, so #0 means left side of detector
     // RETURN: first pair is leftmost coord, second pair is right side of detector
     pair<pair<double, double>, pair<double, double>> GetCurrentDetectorPosition(int viewNum, int detectorNum) {
-        pair<double, double> detectorPanelCenter(0.0, detectorDist);
         pair<double, double> detectorLeftSide(0.0, detectorDist);  //leftmost range of this detector pixel
         pair<double, double> detectorRightSide(0.0, detectorDist); //rightmost range of this detector pixel
 
@@ -54,34 +53,104 @@ public:
     //simulate a fan of photons from source, through subject, and hitting a specific detector
     // viewNum is which 
     // detectorNum is which detector on the panel this fan beam is projected to
+    // calls computeLineIntersections for both lines in the fan beam
+    // combines area to get complete list of pixels with their areas
     void computeFanIntersections(int viewNum, int detectorNum) {
         //compute fanbeam triangle using source position, detector panel position, and which detector on the panel is receiving
         pair<double, double> sourcePosition = GetCurrentSourcePosition(viewNum);
 
         auto detectorPosition = GetCurrentDetectorPosition(viewNum, detectorNum);
 
+        //TODO call computeLineIntersections for both lines
+
         //checking for overlap and pixel area
         // see "Fast and accurate computation of system matrix for area integral model-based algebraic reconstruction technique" in Slack
         double pixelSize = 1.0 / subjectResolution;
+    }
 
-        // from the aforementioned paper:
-        // m = slope
-        // delta is the width of each pixel = 1.0/subjectResolution
-        // S1 = (2d - m*delta)*delta/2
-        // S3 = delta^2 - (m*delta - d)^2/2m
-        // S4 = d^2/2m  
+    //what pixels each line intersects, always computes pixel area below line, never above
+    void computeLineIntersections(pair<double, double> sourcePos, pair<double, double> detectorPos) {
+/*
+    if Line vertical
+
+    if line horizontal
+    
+    if line slope up
+        if m<=1
+        else m>1
+
+        if line-left enters left wall
+        else line-left enters bottom wall
+
+        march along until exit
+
+        if line-right exits right wall
+        else line-right exits top wall
+
+    else line slope down
+        if m<=1
+        else m>1
+
+        if line-right enters right wall
+        else line-right enters bottom wall
+
+        march along until exit
+
+        if line-left exits left wall
+        else line-left exits top wall
+
+    // from the aforementioned paper:
+    // delta = the width of each square pixel (1/subjectResolution), but for our uses, we leave it as *1* to reduce floating pt error
+    // S1 = (2d - m*delta)*delta/2
+    // S3 = delta^2 - (m*delta - d)^2/2m
+    // S4 = d^2/2m
+    
+*/
+
 
         // First pixel intersected by a ray:
+        // (0,0) is CENTERED TOP LEFT
         // n = pixels in each row/column
-        // D = radius of subject, which is a 1x1 square, so D = 0.5
-        // h = -m*D + b + D
-        // i = row index = n - 1 floor(h/delta)
-        // j = 0
+        // m = slope (it's a ratio, no units needed)
+        // b = y-intercept of line, in units of pixels
+        // c = x intercept of line, in units of pixels
+        // D = radius of subject, which is a 1x1 square, so D = 0.5, but this is on the units of pixels, not the whole area, so 0.5 * subjectResolution
+
+        // if -1 > m > 1 ; see pg4
+
+        
+        // if m > 0
+
+        // h_left = height of intersect of left wall, from bottom (in pixels) = -m*D + b + D
+        // w_bot = width of intersect of bottom wall from left (in pixels) = -(1/m)*D + c + D
+
+        //   if 0 < h_left < n
+        //     i = row index = n - 1 - floor(h_left/delta)
+        //     j = 0
+        //     d = h_left - i
+        //   else
+        //     i = 0
+        //     j = column index = n - 1 - floor(w_bot/delta)
+        //     d = w_bot - j
+
+        // else m < 0
+
+        // h_right = height of intersect of right wall, from bottom (in pixels) = m*D + b + D
+        // w_bot = width of intersect of bottom wall from left (in pixels) = (1/m)*D + c + D
+
+        //   if 0 < h_right < n
+        //     i = row index = n - 1 - floor(h_right/delta)
+        //     j = 0
+        //     d = h_right - i
+        //   else
+        //     i = 0
+        //     j = column index = n - 1 - floor(w_bot/delta)
+        //     d = w_bot - j
+        
         // k = i*n + j
-        // d = D*(1-m) + b - (n-1-i)*delta
 
         //"pixel traversing and line-pixel intersection areas calculation algorithm"
-        // OUTPUT: # of pixels intersected by line, their indices, and the area under the ray for that pixel
+        // OUTPUT: # of pixels intersected by line, their indices, and the area *under* the ray for that pixel
         /*
         vector<int> index // stores indices, in order, of the pixels that our ray intersects
         vector<double> area // stores the areas, in order, of the pixels that our ray intersects
@@ -132,31 +201,5 @@ public:
        //NOTES:
        // This alg works when the ray exits a vertical boundary of the image. (see fig 2A) 
        // It may need to be modified according to the paragraphs after the algorithm to handle the case when the line exits a horizontal boundary
-
-       //There also needs to be consideration for if the ray is horizontal, perhaps
     }
 };
-
-/*
-computeFanIntersections - calls computeLineIntersections for both lines in the fan beam
-    computeLineIntersections - what pixels each line intersects
-        Special Cases: if Line vertical/horizontal
-        if line slope up
-            if line-left enters left wall
-            else line-left enters bottom wall
-
-            march along until exit
-
-            if line-right exits right wall
-            else line-right exits top wall
-
-        else line slope down
-            if line-right enters right wall
-            else line-right enters bottom wall
-
-            march along until exit
-
-            if line-left exits left wall
-            else line-left exits top wall
-
-*/
