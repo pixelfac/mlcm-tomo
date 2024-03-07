02abbd15d7ae61ea5518efee07f5c7601d62fa26
pixelfac <n.harris1@ufl.edu>
2024-03-06 21:42:13 -0500

    reorganize slope boolean logic

--- CTScanner.h
+++ CTScanner.h
@@ -76,8 +76,9 @@ public:
     
         //calculate the slope of the line from the source to the detector using y2 - y1/x2-x1
         double slope = (detectorPos.second - sourcePos.second)/(detectorPos.first - sourcePos.first);
-        double b = sourcePos.second - slope*sourcePos.first;
-        double c = sourcePos.first - slope*sourcePos.second;
+        double b = (sourcePos.second - slope*sourcePos.first) * subjectResolution; //y intercept in units of pixels
+        double c = (sourcePos.first - slope*sourcePos.second) * subjectResolution; //x intercept in units of pixels
+        double D = 0.5 * subjectResolution; //half-width/half-height of subject area, in units of pixels
         int delta = 1;
         /*
 
@@ -284,24 +285,9 @@ public:
 
 
                 */
-        else if(slope > 0) //the line is sloping up
-        {
-            /*
-                if m<=1
-                else m>1
 
-                if line-left enters left wall
-                else line-left enters bottom wall
-
-                march along until exit
-
-                if line-right exits right wall
-                else line-right exits top wall
-            */
-
-            // Check if the slope is less than or equal to 1
-            if (slope <= 1)
-            {
+        else if (abs(slope) <= 1) { // -1 <= slope <= 1
+            if (slope > 0) {
                 // Calculate the height of intersect of left wall
                 double h_left = (-slope * 0.5) + b + 0.5; // D = 0.5
 
@@ -378,105 +364,10 @@ public:
                     }
                 }
             }
-            else // Slope is greater than 1
-            {
-                // Calculate the height of intersect of right wall
-                double h_right = (slope * 0.5) + b + 0.5; // D = 0.5
-
-                // Calculate the width of intersect of bottom wall from left
-                double w_bot = ((1 / slope) * 0.5) + c + 0.5; // D = 0.5
-
-                int i, j;
-                double d;
-
-                // Check if h_right is within the range of pixels
-                if (h_right >= 0 && h_right < subjectResolution)
-                {
-                    // Start from the right wall
-                    i = subjectResolution - 1 - floor(h_right / delta);
-                    j = 0;
-                    d = h_right - i * delta;
-                }
-                else
-                {
-                    // Start from the bottom wall
-                    i = subjectResolution - 1;
-                    j = subjectResolution - 1 - floor(w_bot / delta);
-                    d = w_bot - j * delta;
-                }
-
-                int num = 0;
-                int k = i * subjectResolution + j;
-
-                vector<int> index;
-                vector<double> area;
-
-                // Pixel traversing and line-pixel intersection areas calculation algorithm
-                while (i >= 0 && j < subjectResolution)
-                {
-                    d -= slope * delta;
-                    if (d > delta)
-                    {
-                        d -= delta;
-                        index.push_back(k);
-                        area.push_back(pow(d, 2) / (2 * slope));
-                        num++;
-                        i--;
-                        k -= subjectResolution;
-                        if (i >= 0)
-                        {
-                            index.push_back(k);
-                            area.push_back((2 * d - slope * delta) * delta / 2);
-                            num++;
-                            j++;
-                            k++;
-                        }
-                        else
-                        {
-                            break;
-                        }
-                    }
-                    else if (d < delta)
-                    {
-                        index.push_back(k);
-                        area.push_back((slope * delta - d) * delta / 2);
-                        num++;
-                        j++;
-                        k++;
-                    }
-                    else
-                    {
-                        d = 0;
-                        index.push_back(k);
-                        area.push_back((slope * delta - d) * delta / 2);
-                        j++;
-                        i--;
-                        k = k - subjectResolution + 1;
-                        num++;
-                    }
-                }
-            }
-        }
-        else if(slope < 0) //the line is sloping down
-        {
-            /*
-                if m<=1
-                else m>1
-
-                if line-right enters right wall
-                else line-right enters bottom wall
-
-                march along until exit
-
-                if line-left exits left wall
-                else line-left exits top wall
-            */
-            if (abs(slope) <= 1) // slope <= 1
-            {
+            else { // slope < 0
                 // Determine whether line-left enters the left wall or bottom wall
                 // This depends on the y-intercept of the line.
                 // If b < 0.5, it enters the left wall. Otherwise, it enters the bottom wall.
-                double b = sourcePos.second; // y-intercept of the line
                 double D = 0.5 * subjectResolution; // Radius of the subject
                 double h_left = -slope * D + b + D; // height of intersect of left wall, from bottom
                 int i, j;
@@ -544,76 +435,15 @@ public:
                     }
                 }
             }
-            else // slope > 1
-            {
-                // Similar to the case when the slope is less than 1, but we invert the role of x and y.
-                // So, wherever we used to calculate the left wall, now we calculate the top wall, and vice versa.
-                double D = 0.5 * subjectResolution; // Radius of the subject
-                double w_top = slope * D + c + D; // width of intersect of top wall, from left
-                int j, i;
-                double d;
-                if (0 < w_top && w_top < subjectResolution)
-                {
-                    j = subjectResolution - 1 - floor(w_top / delta);
-                    i = 0;
-                    d = w_top - j;
-                }
-                else
-                {
-                    j = subjectResolution - 1;
-                    double h_right = slope * D + b + D; // height of intersect of right wall, from bottom
-                    i = subjectResolution - 1 - floor(h_right / delta);
-                    d = h_right - i;
-                }
+        }
+        else // abs(slope) > 1
+        {
+            if (slope > 0) { // slope > 1
 
-                // Traverse along the line until exit
-                int num = 0;
-                int k = i * subjectResolution + j;
-                vector<int> index;
-                vector<double> area;
-                while (i < subjectResolution && j >= 0)
-                {
-                    d += (1 / slope) * delta;
-                    if (d > delta)
-                    {
-                        d -= delta;
-                        index.push_back(k);
-                        area.push_back((delta * delta - ((1 / slope) * delta - d) * ((1 / slope) * delta - d)) / (2 / slope));
-                        num++;
-                        j--;
-                        k--;
-                        if (j >= 0)
-                        {
-                            index.push_back(k);
-                            area.push_back(d * delta / 2);
-                            num++;
-                            i++;
-                            k = k + subjectResolution;
-                        }
-                        else
-                        {
-                            break;
-                        }
-                    }
-                    else if (d < delta)
-                    {
-                        index.push_back(k);
-                        area.push_back((delta - d) * delta / 2);
-                        num++;
-                        i++;
-                        k = k + subjectResolution;
-                    }
-                    else
-                    {
-                        d = 0;
-                        index.push_back(k);
-                        area.push_back(delta * delta / 2);
-                        i++;
-                        j--;
-                        k = k + subjectResolution - 1;
-                        num++;
-                    }
-                }
+            }
+            else // slope < -1
+            {
+
             }
         }
     }
