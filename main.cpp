66b2600e39ccedf2aa7a3fb2cce54f6a29c40a79
pixelfac <n.harris1@ufl.edu>
2024-03-13 00:10:27 -0400

    call computelineintersection from main

--- main.cpp
+++ main.cpp
@@ -4,8 +4,23 @@
 #include <utility>
 #include <cmath>
 
+#include "CTScanner.h"
+
 using namespace std;
 
 int main() {
+    CTScanner test(4, 1, 1, 1, 2, 2);
+
+    auto sourcePos = test.GetCurrentSourcePosition(0);
+    auto detectorPos = test.GetCurrentDetectorPosition(0, 1);
+
+    auto results = test.computeLineIntersections(sourcePos, detectorPos.first);
+
     return 0;
 };
+
+/*
+
+
+
+*/
