bbfb42e07289ef510985b24794d6d7832c01976a
pixelfac <n.harris1@ufl.edu>
2024-03-14 12:56:36 -0400

    horizontal tests

--- test/test.cpp
+++ test/test.cpp
@@ -2,18 +2,31 @@
 #include "catch.hpp"
 #include "../CTScanner.h"
 
-TEST_CASE("Horizontal Line Through Pixels", "[ComputeLine]") {
+TEST_CASE("Horizontal Line", "[ComputeLine]") {
     CTScanner test(4, 1, 1, 1, 2, 2);
 
-    auto sourcePos = pair<double, double>(1, 0.1);
-    auto detectorPos = pair<double, double>(-1, 0.1);
+    SECTION("Horizontal Line through pixels") {
+        auto sourcePos = pair<double, double>(1, 0.1);
+        auto detectorPos = pair<double, double>(-1, 0.1);
 
-    map<int, double> row = test.computeLineIntersections(sourcePos, detectorPos);
-    cout << "row[0]" << row[0] << endl;
+        map<int, double> row = test.computeLineIntersections(sourcePos, detectorPos);
 
-    REQUIRE(row.size() == 2);
-    REQUIRE(row[0] == 0.2);
-    REQUIRE(row[1] == 0.2);
+        REQUIRE(row.size() == 2);
+        REQUIRE(row[0] == Approx(0.2));
+        REQUIRE(row[1] == Approx(0.2));
+    }
+
+    SECTION("Horizontal Line between pixels") {
+        auto sourcePos = pair<double, double>(1, 0);
+        auto detectorPos = pair<double, double>(-1, 0);
+
+        map<int, double> row = test.computeLineIntersections(sourcePos, detectorPos);
+
+        REQUIRE(row.size() == 2);
+        REQUIRE(row[0] == Approx(0));
+        REQUIRE(row[1] == Approx(0));
+    }
+
 }
 
 TEST_CASE("Vertical Line Through Pixels", "[ComputeLine]") {
@@ -25,6 +38,6 @@ TEST_CASE("Vertical Line Through Pixels", "[ComputeLine]") {
     map<int, double> row = test.computeLineIntersections(sourcePos, detectorPos);
 
     REQUIRE(row.size() == 2);
-    REQUIRE(row[1] == 0.2);
-    REQUIRE(row[3] == 0.2);
+    REQUIRE(row[1] == Approx(0.2));
+    REQUIRE(row[3] == Approx(0.2));
 }
