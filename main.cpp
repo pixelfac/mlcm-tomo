5b1070c12ea2558cd50c159ef6d0f6112f0f4c4d
pixelfac <n.harris1@ufl.edu>
2024-03-20 23:22:49 -0400

    add timer

--- main.cpp
+++ main.cpp
@@ -9,19 +9,26 @@
 using namespace std;
 
 int main() {
+    auto start = std::chrono::high_resolution_clock::now();
     CTScanner test(4, 1, 1, 1, 2, 128);
 
     auto sourcePos = pair<double, double>(0.5, -0.65);
         auto detectorPos = pair<double, double>(0, -0.3);
 
+    for (int i = 0; i < 64000; i++)
+        auto row = test.computeLineIntersections(sourcePos, detectorPos).first;
+
+
+    auto end = std::chrono::high_resolution_clock::now();
+
+    std::chrono::duration<double> duration = end - start;
+    std::cout << "Execution time: " << duration.count() << " seconds." << std::endl;
 
 
 
-    auto row = test.computeLineIntersections(sourcePos, detectorPos).first;
-
-    for(auto iter = row.begin(); iter != row.end(); iter++) {
-        cout << "Index: " << iter->first << "\tArea: " << iter->second << endl;
-    }
+    // for(auto iter = row.begin(); iter != row.end(); iter++) {
+    //     cout << "Index: " << iter->first << "\tArea: " << iter->second << endl;
+    // }
 
     return 0;
 };
