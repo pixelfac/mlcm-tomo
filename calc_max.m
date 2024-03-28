337dfdd5fe4b1eb23e0490902c811568436c3e07
Chris Comes <qx56jx35@gmail.com>
2024-03-28 16:06:12 -0400

    Update calc and gpu sheet

--- calc_max.m
+++ calc_max.m
@@ -2,7 +2,7 @@ n = 4096; % is the resolution of the image
 r = n * 4; % the distance from the detector to the origin
 os = n * 4; % the distance from the origin to the source
 thetas = linspace(0, 2*pi, 100); 
-delta = 0.8;
+delta = 1;
 
 maxProjLen = (ceil( getMaxProjection(r, os, thetas, n/2) ));
 samplePt   = [-fliplr( 0 : delta : maxProjLen/2 + delta), delta : delta : maxProjLen/2 + delta];
