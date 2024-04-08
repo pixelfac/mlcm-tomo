6b19fc22a95e61a281ab00ab0f650fab5fbb3e8e
pixelfac <n.harris1@ufl.edu>
2024-04-08 16:39:29 -0400

    trim down dotprod fs

--- GPU/shaders/dotproduct.fs
+++ GPU/shaders/dotproduct.fs
@@ -2,17 +2,12 @@
 
 layout(location = 0) out vec4 FragColor;
 
-layout(origin_upper_left, pixel_center_integer) in vec4 gl_FragCoord;
-
 in vec2 UV;
-
-uniform int u_resolution;
-
-out vec4 color;
 
 uniform sampler2D renderedTexture;
 
 void main(){
-    FragColor = texture(renderedTexture, vec2(UV.x, UV.y));
+    FragColor = texture(renderedTexture, UV);
+    // FragColor = vec4(texture(renderedTexture, UV).xy, vec2(1.0,1.0));
     // FragColor = vec4(UV.x, UV.y, 1.0, 0.0);
 }
